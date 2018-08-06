72627947d41d93c4ffa893e7126bb604191d06ec
Sandhya Nagarajan <n.sandhya87@gmail.com>
2018-08-06 13:32:30 -0700

    added stagging server using same security group as jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,15 +40,35 @@ resource "aws_instance" "jenkins" {
   Name = "jenkins-server"
   }
  # 02.08.18 :  you need to tell the EC2 Instance to actually use the new security group.
- 
+
   vpc_security_group_ids = ["${aws_security_group.sg_jenkins.id}"]
 }
 
 /*
+08.06.2018: added stagging servers
+*/
+resource "aws_instance" "stagging" {
+  ami = "ami-ba602bc2"
+  instance_type = "m4.large"
+  key_name        = "${var.keypair_name}"
+  tags {
+  Name = "stagging-server"
+  }
+ # 06.08.18 :  Using the same security group for stagging server as Jenkins
+
+  vpc_security_group_ids = ["${aws_security_group.sg_jenkins.id}"]
+}
+
+
+/*
 08.02.2018:output the public ip of the instance
 */
-output "public_ip" {
-  value = "${aws_instance.jenkins.public_ip}"
+output "jenkins_ip" {
+  value = "${aws_instance.jenkins.public_dns}"
+}
+
+output "stagging_ip" {
+  value = "${aws_instance.stagging.public_dns}"
 }
 
 /*
